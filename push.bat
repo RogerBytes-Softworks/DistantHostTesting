8da36f311a43126154f191055bc6370fbb5c4051
Harry Richmond <rogerbytes@vivaldi.net>
2023-12-07 12:27:34 +0100

    script corrigé

--- push.bat
+++ push.bat
@@ -1,6 +1,14 @@
 @echo off
 
-git add .
-git commit -m "update"
+setlocal enabledelayedexpansion
+
+if not "%~1"=="" (
+  set "message=%~1"
+) else (
+  set "message=no comment"
+)
+
+git add --all
+git commit -m "%message%"
 git push
 cls

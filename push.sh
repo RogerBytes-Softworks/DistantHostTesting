e9303c49c1d9a682448ab37505617baa94cae413
Harry Richmond <rogerbytes@vivaldi.net>
2024-05-12 10:01:34 +0200

    Update

--- push.sh
+++ push.sh
@@ -3,10 +3,16 @@
 if [ -n "$1" ] && [ "$1" != "''" ] && [ "$1" != '""' ]; then
   message="$1"
 else
-  message="no comment"
+  message="Update"
 fi
 
 git add --all
 git commit -m "$message"
 git push
 clear
+
+# TODO
+# captation d'erreur sur le commit
+# captation d'erreur sur le push
+# proposer un git pull (en prevenant qu'il pourra il y avoir des conflit de fichier) s'il y a une erreur lors du push
+# Une option pour commit sans push
